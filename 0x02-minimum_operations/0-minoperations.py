a67d1376b35ba69b7829247633288ed925612915
med.wf <med.wf95@gmail.com>
2024-04-26 16:37:49 +0100

    fix error in past operation ! - finnally i did it. 😍

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,18 +11,18 @@ def minOperations(n: int) -> int:
     """
     if (not isinstance(n, int)) or n <= 1:
         return 0
-    content = 1
-    oldContent = content
-    content *= 2
+
+    # first operation must be (copy all - past).
+    oldContent = 1
+    content = 2
     op = 2
+
     while content < n:
         if n % content == 0:
             oldContent = content
             content *= 2
             op += 2
         else:
-            tmp = content
             content += oldContent
-            oldContent = tmp
             op += 1
     return op
