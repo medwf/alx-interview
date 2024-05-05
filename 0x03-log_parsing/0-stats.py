619f429115b281adeaf51325a7c418f724a4bd75
med.wf <med.wf95@gmail.com>
2024-05-05 16:50:12 +0100

    trying to solve this interview

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,39 +3,46 @@
 this module for interview project log parsing
 """
 from sys import stdin
-from re import match
+from re import fullmatch
+
+
+def printOutput(totalSize, status):
+    """print status numbers"""
+    print(f"File size: {totalSize}")
+    for key, value in sorted(status.items()):
+        print('{}: {}'.format(key, value))
 
 
 def run():
     """run code"""
-    while True:
-        status = {
-            "200": 0,
-            "301": 0,
-            "400": 0,
-            "401": 0,
-            "403": 0,
-            "404": 0,
-            "405": 0,
-            "500": 0
-        }
-        totalSize = 0
-        for _ in range(0, 10):
-            line = stdin.readline()
-            pattern = r'^(\d+\.\d+\.\d+\.\d+) - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] "GET (.+)" (\d{3}) (\d+)$'
-            if match(pattern, line):
-                # ipAddress = match(pattern, line).group(1)
-                # dateTime = match(pattern, line).group(2)
-                # requestedPath = match(pattern, line).group(3)
-                statusCode = match(pattern, line).group(4)
-                responseSize = int(match(pattern, line).group(5))
-                status[statusCode] += 1
-                totalSize += int(responseSize)
-            else:
-                continue
-        print(f"File size: {totalSize}")
-        for key, value in status.items():
-            print('{}: {}'.format(key, value))
+    try:
+        while True:
+            # count = 0
+            status = {
+                "200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0
+            }
+            totalSize = 0
+            for _ in range(10):
+                line = stdin.readline()[:-1]
+                pattern = (
+                    r'^(\d+\.\d+\.\d+\.\d+) - '
+                    r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] '
+                    r'"GET (.+)" '
+                    r'(\d{3}) '
+                    r'(\d+)$'
+                )
+                if fullmatch(pattern, line):
+                    statusCode = fullmatch(pattern, line).group(4)
+                    responseSize = int(fullmatch(pattern, line).group(5))
+                    if statusCode in status:
+                        status[statusCode] += 1
+                    totalSize += int(responseSize)
+                else:
+                    continue
+            printOutput(totalSize, status)
+    except (KeyboardInterrupt, EOFError):
+        printOutput(totalSize, status)
 
 
 if __name__ == '__main__':
