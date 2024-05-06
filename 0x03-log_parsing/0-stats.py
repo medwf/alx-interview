cd46ce358848fefead68dfac11cdb2815c414c2a
med.wf <med.wf95@gmail.com>
2024-05-06 13:27:36 +0100

    trying to solve this problem

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,45 @@
 #!/usr/bin/python3
 """
-this module for interview project log parsing
+Log parsing
 """
 from sys import stdin
-from re import fullmatch
 
 
-def printOutput(totalSize, status):
-    """print status numbers"""
-    print(f"File size: {totalSize}")
-    for key, value in sorted(status.items()):
-        print('{}: {}'.format(key, value))
+def print_stats(stats: dict, file_size: int) -> None:
+    """a methods that print output status"""
+    print("File size: {:d}".format(file_size))
+    for k, v in sorted(stats.items()):
+        if v:
+            print("{}: {}".format(k, v))
 
 
 def run():
     """run code"""
+    filesize, count = 0, 0
+    stats = {
+        "200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0
+        }
     try:
-        while True:
-            # count = 0
-            status = {
-                "200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0
-            }
-            totalSize = 0
-            for _ in range(10):
-                line = stdin.readline()[:-1]
-                pattern = (
-                    r'^(\d+\.\d+\.\d+\.\d+) - '
-                    r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] '
-                    r'"GET (.+)" '
-                    r'(\d{3}) '
-                    r'(\d+)$'
-                )
-                if fullmatch(pattern, line):
-                    statusCode = fullmatch(pattern, line).group(4)
-                    responseSize = int(fullmatch(pattern, line).group(5))
-                    if statusCode in status:
-                        status[statusCode] += 1
-                    totalSize += int(responseSize)
-                else:
-                    continue
-            printOutput(totalSize, status)
-    except (KeyboardInterrupt, EOFError):
-        printOutput(totalSize, status)
+        for line in stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
 
 
 if __name__ == '__main__':
