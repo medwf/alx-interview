759ccdaaa32461cb80952efc23d0afcc363e750c
med.wf <med.wf95@gmail.com>
2024-05-21 17:57:39 +0100

    validUTF8 - check this solution

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,10 +2,27 @@
 """
 This module for method UTF-8 Validation
 """
-from typing import List, Union
+from typing import List
 
 
-def validUTF8(data: List[int]):
+def countBytes(number: int) -> int:
+    """
+    Count bytes in number
+    Args:
+        num <int>: represent byte
+    return:
+        int : number of bytes.
+    """
+    count = 0
+    # 128 == (1 << 7)
+    check = 128
+    while number & check:
+        count += 1
+        check >>= 1
+    return count
+
+
+def validUTF8(data: List[int]) -> bool:
     """
     determines if a given data set represents a valid UTF-8 encoding.
 
@@ -15,7 +32,15 @@ def validUTF8(data: List[int]):
     return:
         True if data is valid UFT8 else False
     """
-    for char in data:
-        if char > 255:
-            return False
-    return True
+    NumBytes = 0
+    for byte in data:
+        if not NumBytes:
+            NumBytes = countBytes(byte)
+            if not NumBytes:
+                continue
+            elif NumBytes == 1 or NumBytes > 4:
+                return False
+        else:
+            if countBytes(byte) != 1:
+                return False
+    return NumBytes == 0
