0cac3a298c2b3f30cc3bd73c07452f0e7c1875ba
med.wf <med.wf95@gmail.com>
2024-06-24 22:32:58 +0100

    make solution by checking left/up/right/down for each time point to piece of land

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""module island_perimeter"""
+from typing import List
+
+
+def island_perimeter(grid: List[List[int]]) -> int:
+    """
+    The perimeter of the island described in grid.
+
+    Args:
+        grid (List(List(int))): 0 represent water, 1 represent land
+
+    Return (int): the perimeter of the island.
+    """
+
+    perimeter = 0
+
+    for row in range(len(grid)):
+        for column in range(len(grid[0])):
+            if grid[row][column]:
+
+                # check left.
+                if not column or not grid[row][column - 1]:
+                    perimeter += 1
+
+                # check up
+                if not row or not grid[row - 1][column]:
+                    perimeter += 1
+
+                # check right
+                if column == len(grid[0]) - 1 or not grid[row][column + 1]:
+                    perimeter += 1
+
+                # check down
+                if row == len(grid) - 1 or not grid[row + 1][column]:
+                    perimeter += 1
+    return perimeter
